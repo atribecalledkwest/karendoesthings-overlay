0a85b1d8a576cc1553491e05faa6e9eb2153082b
atribecalledkwest <c.matthew.w97@gmail.com>
2017-08-16 13:51:37 -0400

    Initial commit, I did a lot of work already but I lost it because I forgot to push so I'm just pushing it all to a public github as I redo it.

---
+++ electron.js
@@ -0,0 +1,32 @@
+'use strict';
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+      app.quit();
+   }
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        useContentSize: true,
+        resizeable: false,
+        fullscreen: false,
+        frame: false
+    });
+    
+    mainWindow.loadURL('http://localhost:9090/graphics/karendoesthings-overlay/index.html');
+
+    // OBS doesn't enjoy minimized windows, always keep it open
+    mainWindow.on('minimize', function() {
+        mainWindow.restore();
+    });
+});
