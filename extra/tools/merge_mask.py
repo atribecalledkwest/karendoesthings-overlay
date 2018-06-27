ae6e8376bdace4798b04b86994d967ca0294c41a
Karen West <atribecalledkwest.97@gmail.com>
2018-06-27 05:35:17 -0400

    Update merge_mask.py
    Make it use argparse because that make sense

--- extra/tools/merge_mask.py
+++ extra/tools/merge_mask.py
@@ -1,16 +1,27 @@
-import sys
+from argparse import ArgumentParser
 from PIL import Image
+
+a = ArgumentParser()
+a.add_argument("--input")
+a.add_argument("--mask")
+a.add_argument("--out")
 
 def merge_mask(fp1, fp2):
     # Image
-    i1 = Image.open(fp1)
+    i = Image.open(fp1)
     # Mask
-    i2 = Image.open(fp2)
+    m = Image.open(fp2)
+
+    # Sanity check
+    if not i.size == m.size:
+        a.error("Image and mask sizes are not the same")
+
     # Copy
     c = Image.new("RGBA", i1.size, None)
     c.paste(i1, mask=i2)
     return c
 
-i1, i2, out = sys.argv[1:4]
-img = merge_mask(i1, i2)
-img.save(out)
+p = a.parse_args()
+
+img = merge_mask(p.input, p.mask)
+img.save(p.out)
